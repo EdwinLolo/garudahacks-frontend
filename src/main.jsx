555352d600bca32e81cef030c9c18d627a912e31
Twilight <mario.richie.lim@gmail.com>
2025-07-24 15:22:00 +0700

    files out

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import LandingPage from './Pages/LandingPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
